89e095b59b95d7c7de87bcf3f349c06ad952a974
José Daniel Grayson <josedanielgrayson@proton.me>
2025-11-18 09:57:13 -0500

    feat: add matmul function

--- src/main.s
+++ src/main.s
@@ -1,53 +1,97 @@
-.section .text
-.globl _start
-
-# This program writes the first 20 Fibonacci numbers to memory starting at 0x80000000.
+.text
+.global _start
 
 _start:
-    li      sp, 0xFFFFFFF0
 
-    mv      s0, zero
-    li      s1, 0x80000000
-    li      s2, 20
+    la      a0, mat1
+    la      a1, mat2
+    la      a2, dest
+    li      a3, 3
+    li      a4, 2
+    li      a5, 3
+
+    call    matmul
 
 loop:
-    beq     s0, s2, end
-
-    mv      a0, s0
-    call    fib
-
-    slli    t0, s0, 2
-    add     t0, s1, t0
-    sw      a0, 0(t0)
-
-    addi    s0, s0, 1
-    j       loop
-end:
     ebreak
+    j       loop
 
-fib:
-    li      t0, 2
-    blt     a0, t0, .no_recurse     # if (a0 < 2)
+matmul:
+    # t0 -> i
+    # t1 -> j
+    # t2 -> k
+    # t3 -> sum
+    # s0 -> m1_base
+    # s1 -> dest_base
+    # s2 -> m2_base
 
-    addi    sp, sp, -16
-    sw      ra, 12(sp)
-    sw      s0, 8(sp)
-    sw      s1, 4(sp)
+    # t4, t5 -> temporary values
 
-    mv      s0, a0
+    li      s0, 0 # m1_base = 0
+    li      s1, 0 # dest_base = 0
 
-    addi    a0, s0, -1
-    call    fib
-    mv      s1, a0                  # s1 = fib(n - 1)
+    li      t0, 0 # i = 0
+    fori:
+        bge     t0, a3, fori_done
 
-    addi    a0, s0, -2
-    call    fib
-    add     a0, s1, a0              # a0 = fib(n - 1) + fib(n - 2)
+        li      t1, 0 # j = 0
+        forj:
+            bge     t1, a5, forj_done
 
-    lw      ra, 12(sp)
-    lw      s0, 8(sp)
-    lw      s1, 4(sp)
-    addi    sp, sp, 16
+            li      t3, 0 # sum = 0
+            li      s2, 0 # m2_base = 0
 
-.no_recurse:
+            li      t2, 0 # k = 0
+            fork:
+                bge     t2, a4, fork_done
+
+                add     t4, s0, t2 # t4 = m1_base + k
+                slli    t4, t4, 2  # t4 *= 4
+                add     t4, t4, a0 # t4 += mat1
+                lw      t4, 0(t4)
+
+                add     t5, s2, t1 # t5 = m2_base + j
+                slli    t5, t5, 2  # t5 *= 4
+                add     t5, t5, a1 # t5 += mat2
+                lw      t5, 0(t5)
+
+                # TODO: change for a float multiplication
+                or      t4, t4, t5
+
+                add     t3, t3, t4 # sum += t4
+                add     s2, s2, a5 # m2_base += p
+
+                addi    t2, t2, 1
+                j       fork
+            fork_done:
+
+            add     t4, s1, t1 # t4 = dest_base + j
+            slli    t4, t4, 2  # t4 *= 4
+            add     t4, t4, a2 # t4 += dest
+            sw      t3, 0(t4)
+
+            addi    t1, t1, 1
+            j       forj
+        forj_done:
+
+        add     s0, s0, a4 # m1_base += n
+        add     s1, s1, a5 # dest_base += p
+
+        addi    t0, t0, 1
+        j       fori
+    fori_done:
+
     ret
+
+.data
+
+mat1:
+.word 3, 4
+.word 7, 2
+.word 5, 9
+
+mat2:
+.word 3, 1, 5
+.word 6, 9, 7
+
+dest:
